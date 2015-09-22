27fc4a419a4858bb126800a4b9a1fe2ace85301c
Matthias Berg <matthias.berg@live.se>
2015-09-22 13:23:40 +0200

    Distortion working

--- Matlab/Project.m
+++ Matlab/Project.m
@@ -1,18 +1,20 @@
 %% Projekt
 clear all; clc; close all;
 % Ladda in ljudfil
-    filename = 'test.wav';
+    filename = 'My_life_is_potato.wav';
     [y,Fs] = audioread(filename); % y = audio data, Fs = Hertz
-   	
-    %soundsc(y,Fs);
+   	%player = audioplayer(y, Fs);
     
-    yLeft = y(1:1000,1);
-    yRight = y(1:1000,2);
+    
     y(1:1000,:)
 %% Distortion
     distGain = 0.5 % amount of distortion added to signal -1 < a < 1 
-    y = distortion(y(1:1000,:), distGain);
-    
+    y = distortion(distGain , y);
+    player = audioplayer(y, Fs);
+    play(player);
+    pause(5);
+    stop(player);
+
 %% Delay
     
 
